b7fd2606f7784b2abd6974ce533198d16ffe0332
Luca Cozzi <cozziluca1@gmail.com>
2025-09-15 19:24:32 +0200

    caricato def e aggiunto funzioni.2

--- pio.py
+++ pio.py
@@ -1,8 +1,13 @@
 ciao = 12 
+bella=(ciao,12)
 ola = 23
 
 if ciao == 123:
     print(ciao)
 else:
     print(ola)
-    
+
+
+def bella(parama1,parama2):
+    risultato = parama1 + parama2
+    print(risultato)
