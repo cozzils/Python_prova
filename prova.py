f6f354951e1298e909f483e63bc006245c71ebac
Luca Cozzi <cozziluca1@gmail.com>
2025-09-19 09:09:57 +0200

    finta applicazione e aggiutna funzione input

--- prova.py
+++ prova.py
@@ -8,8 +8,16 @@ class pizzeria:
         self.telefono = telefono
         self.indirizzo = indirizzo
 
+    def add_menu_item(self, pizza):
+        self.menu.append(pizza)
+        print(f"Pizza '{pizza}' aggiunta al menu.")
+
+
+
 
 Da_mario = pizzeria("Pizzeria da Mario", 50, menu,"damario@bella", 1234567890, "Via Roma 1")
+pizza = input("Inserisci il nome della pizza da aggiungere al menu: ")
+Da_mario.add_menu_item(pizza)
 
 print(f"Nome pizzeria: {Da_mario.nome} - Coperti: {Da_mario.Ncoperti} - Menu: {Da_mario.menu} - Email: {Da_mario.email} - Telefono: {Da_mario.telefono} - Indirizzo: {Da_mario.indirizzo}")
 
